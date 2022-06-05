fb5f9b2564a65a2675c78873c3839ea6725edf8c
Jose <lunaxp5@gmail.com>
2022-06-04 21:44:41 -0400

    inciando el proyecto

---
+++ src/componentes/Testimonio.js
@@ -0,0 +1,16 @@
+import React from 'react'
+ 
+ 
+const Component = ()=>{
+ 
+ return (
+    <div className="contenedor-testimonio">
+        <img 
+        className="imagen-testimonio" 
+        src={require("../imagenes/testimonio-emma.jpg")} 
+        alt="foto de emma"/>
+    </div>
+ );
+ }
+ 
+export default Component
