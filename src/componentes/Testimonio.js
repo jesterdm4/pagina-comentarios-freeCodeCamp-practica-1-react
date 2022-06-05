5e27e1c6a7213922dca2d4eeb55d4807dc0b3145
Jose <lunaxp5@gmail.com>
2022-06-05 09:01:37 -0400

    añadido el primer componente de emma

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,16 +1,30 @@
 import React from 'react'
  
  
-const Component = ()=>{
+const Testimonio = ()=>{
  
  return (
-    <div className="contenedor-testimonio">
-        <img 
-        className="imagen-testimonio" 
-        src={require("../imagenes/testimonio-emma.jpg")} 
-        alt="foto de emma"/>
-    </div>
- );
+        <div className="contenedor-testimonio">
+            <img 
+            className="imagen-testimonio" 
+            src={require("../imagenes/testimonio-emma.jpg")} 
+            alt="foto de emma"/>
+
+            <div className="contenedor-texto-testimonio">
+                <p className="texto-testimonio">
+                    Emma Bostian
+                </p>
+                <p className="cargo-testimonio">
+                    artificier@ de Forgoten Realms
+                </p>
+                <p className="texto-testimonio">
+                    nacio mujer pero luego se convirtio en una persona muy buena, gracias a poder verse como el viejo que siempre quiso ser
+                </p>
+            </div>
+
+        </div>
+    );
+
  }
  
-export default Component
+export default Testimonio;
